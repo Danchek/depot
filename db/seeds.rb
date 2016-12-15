3307454fb55a776cef7ee638be48eb16ccd7032a
Danchek <denisdotsenko7@gmail.com>
2016-12-15 17:47:21 +0200

    add validations and tests (chapter 8)

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,13 +24,13 @@ Product.create!(title: 'Programming Ruby 1.9 & 2.0',
 
 Product.create!(title: 'Rails Test Prescriptions',
                 description:
-                  "<p>
+                  '<p>
                     <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
                     Rails applications, covering Test-Driven Development from both a
                     theoretical perspective (why to test) and from a practical perspective
                     (how to test effectively). It covers the core Rails testing tools and
                     procedures for Rails 2 and Rails 3, and introduces popular add-ons,
                     including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
-                  </p>",
+                  </p>',
                 image_url: 'rtp.jpg',
                 price: 34.95)
